43aa1ea800d6a2a8a05341606be909ba823aa60d
Dmitriy Dolovov <Dmitriy.Dolovov@jetbrains.com>
2018-11-07 15:27:24 +0300

    K/N: Re-enable specific EPs for CLion
    Issue #KT-26717

--- idea/idea-native/src/org/jetbrains/kotlin/ide/konan/KotlinNativeApplicationComponent.kt
+++ idea/idea-native/src/org/jetbrains/kotlin/ide/konan/KotlinNativeApplicationComponent.kt
@@ -16,13 +16,5 @@ class KotlinNativeApplicationComponent : ApplicationComponent {
 
     override fun initComponent() {
         FileTypeManager.getInstance().associateExtension(ArchiveFileType.INSTANCE, KLIB_FILE_EXTENSION)
-
-        // TODO: Move this to Kotlin/Native plugin for CLion and AppCode (see KT-26717):
-//        val extensionPoint = Extensions.getRootArea().getExtensionPoint(TipAndTrickBean.EP_NAME)
-//        for (name in arrayOf("Kotlin.html", "Kotlin_project.html", "Kotlin_mix.html", "Kotlin_Java_convert.html")) {
-//            TipAndTrickBean.findByFileName(name)?.let {
-//                extensionPoint.unregisterExtension(it)
-//            }
-//        }
     }
 }
